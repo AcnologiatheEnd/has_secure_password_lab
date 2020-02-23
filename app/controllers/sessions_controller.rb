a5943d5855c0a58633b6a23ca9f2cd42686691e8
Winston  Chung <18.chung.winston@ctkprep.org>
2020-02-23 06:04:01 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,9 @@ class SessionsController < ApplicationController
   end
   
   def create
-    #binding.pry
     @user = User.find_by(name: params[:user][:name])
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
-      
       redirect_to 'welcome/home'
     else
       flash[:error] = "yea error mate"
